2d0b0851a7f335b13d94a427cefdf51c859a638d
Etienne Caleron <enn.calderon@hotmail.com>
2026-01-28 14:55:17 -0600

    Rename 'base' on vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Stress-Starain_Curve_Project/',
+  base: '/Stress-Strain_Curve_Project/',
 })
